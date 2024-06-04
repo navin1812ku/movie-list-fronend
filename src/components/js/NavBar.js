33a2c4e95b382eecbd596828f5fc9bfb14affe73
navin1812ku <navin1812ku@gmail.com>
2024-06-04 09:00:40 +0530

    Updated CSS v4

--- src/components/js/NavBar.js
+++ src/components/js/NavBar.js
@@ -5,7 +5,12 @@ function NavBar() {
     return (
         <div>
             <nav className="navbar">
-                <div className="navbar-logo">MENU
+                <div className="navbar-logo">
+                    <div className="hamburger-button">
+                        <div className="line"></div>
+                        <div className="line"></div>
+                        <div className="line"></div>
+                    </div>
                     <div className="dropdown-content">
                         <Link className="navbar-link" to="/logined/landingPage/one piece">Search Movie</Link>
                         <Link className="navbar-link" to="/logined/searchMovieList">Search Movie List</Link>
@@ -13,6 +18,7 @@ function NavBar() {
                         <Link className="navbar-link" to="/logined/logout">Logout</Link>
                     </div>
                 </div>
+                <h1>Film Fetch</h1>
             </nav>
             <Outlet />
         </div>
